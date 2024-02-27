bd132acd525db611301da154a803071cf686dde5
Ekiz24 <tkboxnn@gmail.com>
2024-02-27 15:21:34 +0200

    Initial commit

---
+++ Assets/UnityTechnologies/Let's Try Shooter/Scripts/CompletedScripts/ShootableBox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootableBox : MonoBehaviour {
+
+	public int currentHealth = 3;
+
+	public void Damage(int damageAmount)
+	{
+		currentHealth -= damageAmount;
+
+		if (currentHealth <= 0) 
+		{
+			gameObject.SetActive (false);
+		}
+	}
+}
